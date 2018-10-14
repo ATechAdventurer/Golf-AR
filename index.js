45553781e31030c16ffd58216c4663d67ed96c79
Cameron Steele <cam.steeleis@gmail.com>
2018-10-13 20:28:19 -0500

    Added Boilerplate

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const io = require('socket.io')(80);
 
+var player1 = [];
+var player2 = [];
+
+io.on('connection', function(socket){
+    socket.on('update', function(msg){
+        var {construction, shot} = msg;
+        
+        io.emit('update', {player1, player2});
+    })
+})
 
